b7c78d4ac6c843449929d384e6aa0e8fdb2cc531
K_ROCK_ <75164778+CHOI-K-ROCK@users.noreply.github.com>
2024-05-29 15:59:57 +0900

    -

---
+++ src/screens/WriteThanksScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+const WriteThanksScreen = () => {
+    return (
+        <View>
+            <Text>WriteThanksScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default WriteThanksScreen;
