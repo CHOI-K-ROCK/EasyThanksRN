013ca488612ec5480916f528b8a8a3d2fd4c87bc
K_ROCK_ <75164778+CHOI-K-ROCK@users.noreply.github.com>
2024-06-03 17:34:01 +0900

    - SafeAreaView 스크린 전환 시 깜빡임 수정

--- src/components/common/SafeAreaView.tsx
+++ src/components/common/SafeAreaView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { ColorValue, StyleSheet, ViewStyle } from 'react-native';
-import { SafeAreaViewProps, SafeAreaView as CTXSafeAreaView } from 'react-native-safe-area-context';
+import { ColorValue, StyleSheet, View, ViewStyle } from 'react-native';
+import { SafeAreaViewProps, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import useCustomTheme from '../../hooks/useCustomTheme';
 
@@ -17,6 +17,7 @@ type ExtendedStyleProps = ViewStyle & {
 const SafeAreaView = (props: Props) => {
     const { style, ...restProps } = props;
     const { colors } = useCustomTheme();
+    const { top, left, right, bottom } = useSafeAreaInsets();
 
     const flattenStyles = (StyleSheet.flatten(style) || {}) as ExtendedStyleProps;
 
@@ -24,28 +25,40 @@ const SafeAreaView = (props: Props) => {
     const topAreaBackgroundColor = flattenStyles.topAreaBackgroundColor || backgroundColor;
     const bottomAreaBackgroundColor = flattenStyles.bottomAreaBackgroundColor || backgroundColor;
 
+    /**
+     * View 이용사유
+     * 스크린 전환 간에 깜빡이는 현상이 있어 View + useSafeAreaInsets 사용으로 해결
+     */
     return (
         <>
-            <CTXSafeAreaView
-                edges={['top']}
+            {/* 상단 SafeAreaView backgroundColor */}
+            <View
                 style={[
                     {
                         backgroundColor: topAreaBackgroundColor || backgroundColor,
+                        paddingTop: top,
                     },
                 ]}
             />
-            {/* 메인 SafeAreaView backgroundColor */}
-            <CTXSafeAreaView
-                edges={['left', 'right']}
-                style={[{ backgroundColor: backgroundColor }, styles.safeAreaView, style]}
+            {/* 메인 컨텐츠 영역 backgroundColor */}
+            <View
+                style={[
+                    {
+                        backgroundColor: backgroundColor,
+                        paddingRight: right,
+                        paddingLeft: left,
+                    },
+                    styles.safeAreaView,
+                    style,
+                ]}
                 {...restProps}
             />
-            {/* 상단 SafeAreaView backgroundColor */}
-            <CTXSafeAreaView
-                edges={['bottom']}
+            {/* 하단 SafeAreaView backgroundColor */}
+            <View
                 style={[
                     {
                         backgroundColor: bottomAreaBackgroundColor || backgroundColor,
+                        paddingBottom: bottom,
                     },
                 ]}
             />
